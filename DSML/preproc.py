4f2c3bc1c7d5672d0dc6e7ea2b709086a10854a1
kmowledge <116028665+kmowledge@users.noreply.github.com>
2025-09-26 21:47:57 +0200

    refactor preproc.py; divide into train-test subsets.

--- DSML/preproc.py
+++ DSML/preproc.py
@@ -6,40 +6,41 @@ from pathlib import Path
 from kaggle.api.kaggle_api_extended import KaggleApi
 from loguru import logger
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-from DSML.config import DATASET, PROCESSED_DATA_DIR, RAW_DATA_DIR
+from DSML.config import DATASET, DATASET_FILE_PATH, PROCESSED_DATA_DIR, RAW_DATA_DIR
+
 
 def get_raw_data(dataset:str=DATASET)->None:
     """Download dataset from Kaggle."""
     api = KaggleApi()
     api.authenticate()
     download_folder = Path(RAW_DATA_DIR)
-    api.dataset_download_files(dataset, path=str(download_folder), unzip=False)
+    api.dataset_download_files(dataset, path=str(download_folder), unzip=True)
     logger.info(f"RAW_DATA_DIR is: {RAW_DATA_DIR}")
 
 
-def preprocess_df(file:str|Path)->str|Path:
-    """Preprocess datasets."""
-    _, file_name = os.path.split(file)
-    df = pd.read_csv(file)
+def preprocess_df(data:str|Path=DATASET_FILE_PATH)->None:
+    """Create target feature and split dataset into train, test subsets."""
+    logger.info(f"DATASET_FILE_PATH is: {data}")
+    _, file_name = os.path.split(data)
 
-    df['Alcoholic'] = ((df['Alcohol_Weekends'].isin(['High', 'Very High']) | df['Alcohol_Weekdays'].isin(['High', 'Very High']))).astype(int)
-    df = df.drop(['Alcohol_Weekdays', 'Alcohol_Weekends'], axis=1)
-
+    df = pd.read_csv(data)
+    df["Alcoholic"] = (df["Alcohol_Weekends"].isin(["High", "Very High"]) | df["Alcohol_Weekdays"].isin(["High", "Very High"])).astype(int)
+    df = df.drop(["Alcohol_Weekdays", "Alcohol_Weekends"], axis=1)
+    y = df.pop("Alcoholic")
+    X = df
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
-    outfile_path = PROCESSED_DATA_DIR / file_name
-    df.to_csv(outfile_path, index=False)
 
-    return outfile_path
+    X_train.assign(target=y_train).to_csv(PROCESSED_DATA_DIR / "train.csv", index=False)
+    X_test.assign(target=y_test).to_csv(PROCESSED_DATA_DIR / "test.csv", index=False)
 
 
 if __name__=="__main__":
     # get the train and test sets from default location
-    logger.info("getting datasets")
+    logger.info("downloading dataset")
     get_raw_data()
 
-    # preprocess both sets
-    logger.info("preprocessing train.csv")
-    preprocess_df(RAW_DATA_DIR / "train.csv")
-    logger.info("preprocessing test.csv")
-    preprocess_df(RAW_DATA_DIR / "test.csv")
+    logger.info("preprocessing dataset")
+    preprocess_df()
